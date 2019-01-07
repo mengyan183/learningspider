10e3ec591260a4c2393d5872050808cc6bcd865a
guoxing <guoxing@jiehun.com.cn>
2019-01-07 16:32:41 +0800

    urllib test

---
+++ static_html_spider/urllib_useage_test/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: UTF-8 -*-
+# 引入urllib
+from urllib import request
+# 引入requests
+import requests
+
+"""
+   @author guoxing
+   @date 1/7/2019 4:11 PM 
+   @since 1.0.0 
+"""
+
+def urllib_first_test():
+    """
+       @author guoxing
+       @date 1/7/2019 4:13 PM
+       @since 1.0.0
+       Returns:
+    """
+    print("")
