3139f2916233f81c0757650b5bd9cc8091725dc0
guoxing <guoxing@jiehun.com.cn>
2019-01-11 15:31:15 +0800

    完成对于民政局县和县级以上数据区域数据爬取; one issue : 对于直辖市需要补充市辖区数据

--- static_html_spider/states_gover/mcaxzqh/areacode_mca_spider.py
+++ static_html_spider/states_gover/mcaxzqh/areacode_mca_spider.py
@@ -3,6 +3,10 @@
 Copyright 2019 [xingguo] All rights Reserved.
 """
 from enum import Enum
+from urllib import request
+
+import chardet
+from bs4 import BeautifulSoup
 
 """
     中国民政部县以上(包含县)区划代码爬取
@@ -52,7 +56,7 @@ class Area:
         self.level = level
 
 
-def mca_area_code_spider(Area):
+def mca_area_code_spider():
     """
         民政部行政区域爬取
         TODO:
@@ -61,4 +65,48 @@ def mca_area_code_spider(Area):
        @since 1.0.0
     """
     area_list = []
+    url = "http://www.mca.gov.cn/article/sj/xzqh/2018/201804-12/20181101021046.html"
+    response = request.urlopen(url)
+    # 解析响应
+    read = response.read()
+    detect = chardet.detect(read)
+    # 读取信息并解码
+    html = read.decode(detect["encoding"], "ignore")
+    # 解析html
+    soup = BeautifulSoup(html, "html.parser")
+    tr_list = soup.find_all("tr")
+    for tr in tr_list:
+        tr_soup = BeautifulSoup(str(tr), "html.parser")
+        td_list = tr_soup.find_all("td")
+        if td_list is not None and len(td_list) > 2:
+            area_code = td_list[1].text
+            area_name = td_list[2].text
+            if area_code is not None and area_name is not None and str(area_code) and str(area_name) and str(
+                    area_code).isdigit():
+                area = Area(area_name, area_code, None, None)
+                area_list.append(area)
     return area_list
+
+
+over_country_area_list = mca_area_code_spider()
+print(len(over_country_area_list))
+
+# 订正数据,对数据进行分级处理; 县和县以上数据area_code全为6位纯数字;省级行政区XX0000格式,地级行政区XXXX00格式,县级行政区XXXXXX格式
+fix_area_code = []
+# TODO:对于直辖市缺少市辖区数据
+for area in over_country_area_list:
+    code = area.code
+    # 省级行政区
+    if str(code).endswith("0000"):
+        area.level = AreaLevel.PROVINCIAL.value
+        area.parent_code = 0
+        # 地级行政区
+    elif str(code).endswith("00"):
+        area.level = AreaLevel.PREFECTURE.value
+        area.parent_code = str(code)[:2] + "0000"
+    else:
+        # 县级行政区
+        area.level = AreaLevel.COUNTRY.value
+        area.parent_code = str(code)[:4] + "00"
+    print(area.__dict__.values().__str__())
+    fix_area_code.append(area)
